17638fea72f99bd3c35059945065e540717e3a41
Noémie Lehuby <noemie.lehuby@openmailbox.org>
2017-08-30 15:16:42 +0200

    Corrections diverses

--- exemple/data/make.sh
+++ exemple/data/make.sh
@@ -2,10 +2,11 @@
 # verifier les paramètres de connexion à la base postgres
 USER=xxx
 DB=horodateurs
+HOST=xx
 # a modifier et decommenter en cas d'identification par mots de passe
 #export PGPASSWORD=
-psql -c "create database $DB" -U $USER
-psql -d $DB -v DIR="$PWD" -f init.sql -U $USER
-ogr2ogr -f PostgreSQL PG:"dbname=$DB user=$USER" ./raw_data/horodateurs-mobiliers.shp
-psql -d $DB -f ../../postgis_functions.sql -U $USER
-psql -d $DB -f make.sql -U $USER
+psql -c "create database $DB" -U $USER -h $HOST
+psql -d $DB -v DIR="$PWD" -f init.sql -U $USER -h $HOST
+ogr2ogr -f PostgreSQL PG:"dbname=$DB user=$USER host=$HOST" ./raw_data/horodateurs-mobiliers.shp
+psql -d $DB -f ../../postgis_functions.sql -U $USER -h $HOST
+psql -d $DB -f make.sql -U $USER -h $HOST
